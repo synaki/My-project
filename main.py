1ab93e44d1539d28a77fca9c103a0a1c3407e14b
synaki <b8051634@newcastle.ac.uk>
2019-03-04 11:36:17 +0000

    adding inputs to tn7 transposon

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def user_input():
     #to give the user a outlook of input file format
     input_genome = input("please insert the genome to be edited :")
     # Gives access to the user to import their genome of interest
-
     for sequence in SeqIO.parse(input_genome, "fasta"):
-        print(sequence.seq)
+        print("The length of the given input genome sequence is ")
         print(len(sequence))
     for sequence in SeqIO.parse(input_genome, "gb"):
-        print(sequence.seq)
+        print("The length of the given input genome sequence is ")
         print(len(sequence))
 
-
-
-
-
-
+    TnsA = input("please insert the TnsA gene :")
+    for tnsA in SeqIO.parse(TnsA, "fasta"):
+        
+    TnsB = input("please inser the TnsB gene :")
+    TnsC = input("please insert the TnsC gene :")
+    TnsD = input("please insert the TnsD gene :")
 
 
 user_input()
